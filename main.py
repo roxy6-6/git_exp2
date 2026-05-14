9915fe8bc46cd7139b4fe2ca6c4e90b4d7f11213
roxy6-6 <2353577975@qq.com>
2026-05-15 00:18:42 +0800

    Initialize Library Borrow System application

---
+++ main.py
@@ -0,0 +1,15 @@
+APP_NAME = "Library Borrow System"
+APP_VERSION = "1.0.0"
+CURRENT_BRANCH_FEATURE = "B1_BASE_FEATURE"
+WELCOME_MESSAGE = "欢迎使用基础版图书借阅管理系统"
+
+
+def start_app():
+    print(APP_NAME)
+    print(APP_VERSION)
+    print(WELCOME_MESSAGE)
+    print("当前功能标识：", CURRENT_BRANCH_FEATURE)
+
+
+if __name__ == "__main__":
+    start_app()
